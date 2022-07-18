3df190e777ef6c72de56424002ed1abdd25de699
Lagend <105041466+Lagendking@users.noreply.github.com>
2022-07-18 07:56:12 -0400

    Update LibScript.js

--- LibScript.js
+++ LibScript.js
@@ -1,9 +1,11 @@
 const KEYS = ["CKey_L09a0P", "CKey_SoAc55"]
+let db = false
 
 function checkPass(){
     var Key = document.getElementById("EnterKey");
-    if(KEYS.includes(Key.value)) {
+    if(KEYS.includes(Key.value) && db == false) {
         alert("Correct Key!")
+        db = true
         window.location.replace("LibraryInfo");
     }
 }
